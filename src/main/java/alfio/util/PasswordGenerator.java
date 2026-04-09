886da73212c7b212d3523af74b3950bd14114bdb
txt <iantee1201@1utar.my>
2026-04-09 16:21:09 +0800

    add character

--- src/main/java/alfio/util/PasswordGenerator.java
+++ src/main/java/alfio/util/PasswordGenerator.java
@@ -58,7 +58,8 @@ public final class PasswordGenerator {
         chars.add('(');
         chars.add(')');
         chars.add('=');
-
+        chars.add('?');
+        
         PASSWORD_CHARACTERS = ArrayUtils.toPrimitive(chars.toArray(new Character[0]));
         DEV_MODE = Arrays.stream(Optional.ofNullable(System.getProperty("spring.profiles.active")).map(p -> p.split(",")).orElse(new String[0]))
             .map(StringUtils::trim)
